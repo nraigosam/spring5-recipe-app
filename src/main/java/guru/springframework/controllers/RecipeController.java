e7e24f60e20d6f5b2f88c72ab1184ccbed5e5788
Nicolas Raigosa <nicolasraigosa@gmail.com>
2023-06-08 15:41:52 -0500

    View Ingredients

--- src/main/java/guru/springframework/controllers/RecipeController.java
+++ src/main/java/guru/springframework/controllers/RecipeController.java
@@ -2,10 +2,12 @@ package guru.springframework.controllers;
 
 import guru.springframework.commands.RecipeCommand;
 import guru.springframework.services.RecipeService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+@Slf4j
 @Controller
 public class RecipeController {
 
@@ -48,7 +50,7 @@ public class RecipeController {
     public String deleteById(@PathVariable String id){
 
 
-        //log.debug("Deleting id: " + id);
+        log.debug("Deleting id: " + id);
 
         recipeService.deleteById(Long.valueOf(id));
         return "redirect:/";
