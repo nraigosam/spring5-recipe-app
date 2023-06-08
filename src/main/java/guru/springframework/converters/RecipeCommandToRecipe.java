9184336035dd50ba52ee687c6820f8f851b8cb7e
Nicolas Raigosa <nicolasraigosa@gmail.com>
2023-06-08 16:02:30 -0500

    Show Ingredient

--- src/main/java/guru/springframework/converters/RecipeCommandToRecipe.java
+++ src/main/java/guru/springframework/converters/RecipeCommandToRecipe.java
@@ -39,7 +39,9 @@ public class RecipeCommandToRecipe implements Converter<RecipeCommand, Recipe> {
         recipe.setServings(source.getServings());
         recipe.setSource(source.getSource());
         recipe.setUrl(source.getUrl());
-        recipe.setNotes(notesConverter.convert(source.getNotes()));
+        if (source.getNotes() != null){
+            recipe.setNotes(notesConverter.convert(source.getNotes()));
+        }
 
         if (source.getCategories() != null && source.getCategories().size() > 0){
             source.getCategories()
